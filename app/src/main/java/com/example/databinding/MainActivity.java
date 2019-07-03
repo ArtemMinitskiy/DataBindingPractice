2979997780c1cc800109696a621e6821a1bdc613
rukok <rukokuro@gmail.com>
2019-07-03 15:38:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.databinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
+
+    }
+}
